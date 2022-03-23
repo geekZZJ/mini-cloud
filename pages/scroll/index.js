21a20f7948cbe2683a05b83b74418282123bb31c
zzj <2461927976@qq.com>
2022-03-24 00:30:51 +0800

    scroll-view

--- pages/scroll/index.js
+++ pages/scroll/index.js
@@ -15,6 +15,10 @@ Page({
 
   },
 
+  onScroll(e) {
+    // console.log(e.detail.scrollTop, e.detail.scrollLeft, e.detail.scrollHeight, e.detail.scrollWidth)
+  },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -40,27 +44,6 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
 
   }
 })
