cfc4eb83af5b07de7aecb26a86f1600ad55a693e
zzj <2461927976@qq.com>
2022-03-26 21:53:01 +0800

    体验picker组件

---
+++ pages/picker/index.js
@@ -0,0 +1,61 @@
+// pages/picker/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    array: ['美国', '中国', '巴西', '日本'],
+    index: 0,
+    index1: 1,
+    region: ['广东省', '广州市', '海珠区'],
+    customItem: '全部'
+  },
+
+  bindPickerChange: function (e) {
+    this.setData({
+      index: e.detail.value
+    })
+  },
+
+  bindRegionChange: function (e) {
+    this.setData({
+      region: e.detail.value
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  }
+})
